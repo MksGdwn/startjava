ede85e9d5db0f2b5637ed592ab6b0256d4de203b
MksGdwn <mksgdwn@gmail.com>
2021-12-16 08:12:19 +0500

    Changed tabs to spaces

--- lesson_1/Calculator.java
+++ lesson_1/Calculator.java
@@ -1,28 +1,28 @@
 public class Calculator {
-	public static void main(String[] args) {
-		int a = 10;
-		int b = 2;
-		String operation = "+";
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 2;
+        String operation = "+";
 
-		switch(operation) {
-			case "+":
-				System.out.println(a + b);
-				break;
-			case "-":
-				System.out.println(a - b);
-				break;
-			case "*":
-				System.out.println(a * b);
-				break;
-			case "/":
-				System.out.println(a / b);
-				break;
-			case "^":
-				System.out.println(Math.pow(a, b));
-				break;
-			case "%":
-				System.out.println(a % b);
-				break;
-		}
-	}
+        switch(operation) {
+            case "+":
+                System.out.println(a + b);
+                break;
+            case "-":
+                System.out.println(a - b);
+                break;
+            case "*":
+                System.out.println(a * b);
+                break;
+            case "/":
+                System.out.println(a / b);
+                break;
+            case "^":
+                System.out.println(Math.pow(a, b));
+                break;
+            case "%":
+                System.out.println(a % b);
+                break;
+        }
+    }
 }
--- lesson_1/Cycle.java
+++ lesson_1/Cycle.java
@@ -1,30 +1,30 @@
 public class Cycle {
-	public static void main(String[] args) {
-		for(int i = 0; i <= 20; i++) {
-			System.out.print(i + " ");
-		}
+    public static void main(String[] args) {
+        for(int i = 0; i <= 20; i++) {
+            System.out.print(i + " ");
+        }
 
-		System.out.println();
+        System.out.println();
 
-		int number = 6;
-		while(number >= -6) {
-			System.out.print(number + " ");
-			number -= 2;
-		}
+        int number = 6;
+        while(number >= -6) {
+            System.out.print(number + " ");
+            number -= 2;
+        }
 
-		System.out.println();
+        System.out.println();
 
-		number = 10;
-		int result = 0;
-		do {
-			if (number % 2 != 0) {
-				result += number;
-			}
+        number = 10;
+        int result = 0;
+        do {
+            if (number % 2 != 0) {
+                result += number;
+            }
 
-			number++;
+            number++;
 
-		} while(number < 20);
+        } while(number < 20);
 
-		System.out.println(result);
-	}
+        System.out.println(result);
+    }
 }
