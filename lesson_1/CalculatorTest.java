0ca3b370475c079e7c862d5f76ffe0269d0d0a2c
MksGdwn <mksgdwn@gmail.com>
2021-12-19 17:34:32 +0500

    Class CalculatorTest changed

--- lesson_1/CalculatorTest.java
+++ lesson_1/CalculatorTest.java
@@ -4,17 +4,27 @@ public class CalculatorTest {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Enter first number: ");
-        int number1 = scanner.nextInt();
-        scanner.nextLine();
+        while(true) {
+            System.out.print("Enter first number: ");
+            int number1 = scanner.nextInt();
+            scanner.nextLine();
 
-        System.out.print("Enter a sign for a mathematical operation: ");
-        String symbol = scanner.nextLine();
+            System.out.print("Enter a sign for a mathematical operation: ");
+            String symbol = scanner.nextLine();
 
-        System.out.print("Enter second number: ");
-        int number2 = scanner.nextInt();
+            System.out.print("Enter second number: ");
+            int number2 = scanner.nextInt();
+            scanner.nextLine();
 
-        Calculator calculator = new Calculator();
-        calculator.calculate(number1, number2, symbol);
+            Calculator calculator = new Calculator();
+            calculator.calculate(number1, number2, symbol);
+
+            System.out.print("Do you want to continue calculating? [yes/no]: ");
+            String userAnswer = scanner.nextLine();
+
+            if (!userAnswer.equals("yes")) {
+                break;
+            } 
+        }
     }
 }
