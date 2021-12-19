227e4be0caec82ed38f3f988141714366966156f
MksGdwn <mksgdwn@gmail.com>
2021-12-19 17:04:19 +0500

    Add wolf class

---
+++ lesson_2/Wolf.java
@@ -0,0 +1,71 @@
+public class Wolf {
+    private boolean isMale;
+    private String name;
+    private double weight;
+    private int age;
+    private String color;
+
+    public boolean getIsMale() {
+        return this.isMale;
+    }
+
+    public void setIsMale(boolean isMale) {
+        this.isMale = isMale;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getWeight() {
+        return this.weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public void setAge(int age) {
+        if (age > 8) {
+            System.out.println("Incorrect age");
+        } else {
+            this.age = age;
+        }
+    }
+
+    public String getColor() {
+        return this.color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void walk() {
+        System.out.println(name + " is walking");
+    }
+
+    public void sit() {
+        System.out.println(name + " is sitting");
+    }
+
+    public void run() {
+        System.out.println(name + " is running");
+    }
+
+    public void howl() {
+        System.out.println(name + " is howling");
+    }
+
+    public void hunt() {
+        System.out.println(name + " is hunting");
+    }
+}
---
+++ lesson_2/WolfTest.java
@@ -0,0 +1,22 @@
+public class WolfTest {
+    public static void main(String[] arg) {
+        Wolf wolf = new Wolf();
+        wolf.setIsMale(true);
+        wolf.setName("Gera");
+        wolf.setWeight(34.0);
+        wolf.setAge(2);
+        wolf.setAge(10);
+        wolf.setColor("gray");
+
+        System.out.println(wolf.getIsMale());
+        System.out.println(wolf.getName());
+        System.out.println(wolf.getWeight());
+        System.out.println(wolf.getAge());
+        System.out.println(wolf.getColor());
+        wolf.walk();
+        wolf.sit();
+        wolf.run();
+        wolf.howl();
+        wolf.hunt();
+    }
+}
